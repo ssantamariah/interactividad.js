d7527893e5d125e377617d25f72e4fd6490b1f5d
Sarah <ssantamaria@centro.edu.mx>
2025-10-08 11:00:26 -0600

    var

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// Así se declara una variable
+var nombre = "Sarah";
+let edad = 23
+const PI = Math.PI: // float - tipo flotante 
+const TAU = Math.PI * 2: // float- tipo flotante
+
+// Valor booleano - Tipos de variable que pueden ser true o false
+let esMayorDeEdad = edad >= 18; // Variable formato camelcase
+
+nombre = "Luis"
+edad = 37;
+console.log("Hola " + nombre);

b1125553b1d88864698523943c75a6269b2c5451
Sarah <ssantamaria@centro.edu.mx>
2025-10-15 11:55:29 -0600

    boton darkmode

--- js/dark-mode.js
+++ js/dark-mode.js
@@ -1,5 +1,31 @@
 const toggleThemeBtn = document.getElementById('toggle-theme-btn');
+const colorPicker = document.getElementById('color-picker');
+const backgroundPicker = document.getElementById('background-picker');
 
-toggleThemeBtn.addEventListener('click', function() {
-    alert("me diste click");
-})
+
+
+function toggleTheme() {
+
+    document.body.classList.toggle('dark');
+    if (document.body.classList.contains('dark')) {
+      toggleThemeBtn.innerHTML = "<i class= 'fa-solid fa-sun'></i>";
+        // toggleThemeBtn.textContent = "Activar Light Mode";
+
+    } else {
+      toggleThemeBtn.innerHTML = "<i class= 'fa-solid fa-moon'></i>";
+        // toggleThemeBtn.textContent = "Activar Dark Mode";
+    }
+}
+toggleThemeBtn.addEventListener('click', function () {
+    toggleTheme();
+
+});
+
+
+colorPicker.addEventListener('change', function () {
+    document.body.style.color = colorPicker.value;
+});
+
+backgroundPicker.addEventListener('change', function () {
+    document.body.style.background = backgroundPicker.value;
+});
